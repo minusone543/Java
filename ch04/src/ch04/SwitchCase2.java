e13a3d32dddaba1caecf2ea2bf9f66f7c8fd8730
minusone543 <minusone543@naver.com>
2020-12-10 16:29:23 +0900

    Java수업 1210

---
+++ ch04/src/ch04/SwitchCase2.java
@@ -0,0 +1,30 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class SwitchCase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("메달의 색상을 영어로 입력해주세요 : ");
+		String medal = sc.next();
+		
+		switch (medal) {
+		case "Gold":
+			System.out.println("금메달 입니다.");
+			break;
+		
+		case "Silver":
+			System.out.println("은메달 입니다.");
+		
+		case "Bronze":
+			System.out.println("동메달 입니다.");
+			
+		default:
+			System.out.println("메달이 없습니다.");
+			break;
+		}
+	}
+
+}
